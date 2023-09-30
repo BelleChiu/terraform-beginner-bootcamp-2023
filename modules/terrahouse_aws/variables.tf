d6e246ce239558567efed5f6a752251e5bfc3016
Belle Chiu <49231741+BelleChiu@users.noreply.github.com>
2023-09-30 22:43:59 +0800

    #32 Only update index.html if a conten version has changed. (#33)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -40,3 +40,14 @@ variable "error_html_filepath" {
     error_message = "The specified file does not exist."
   }
 }
+
+
+variable "content_version" {
+  description = "Content version (positive integer starting at 1)"
+  type        = number
+
+  validation {
+    condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "Content version must be a positive integer starting at 1."
+  }
+}
