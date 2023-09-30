2c596dd3b96a04c846a12e449a6cbdf14512adea
BelleChiu <bellechiu@gmail.com>
2023-09-30 14:42:41 +0000

    #32 Only update index.html if a conten version has changed.

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   }
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
@@ -33,6 +33,11 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
   
   etag = filemd5(var.index_html_filepath)
+  
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [ etag ]
+  }
 }
 
 
@@ -43,6 +48,10 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
 
   etag = filemd5(var.error_html_filepath)
+
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -69,3 +78,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     ]
   })
 }   
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
+}
