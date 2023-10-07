652060cb27c1e673a900d2745be40c1d34e4a5f0
Belle Chiu <49231741+BelleChiu@users.noreply.github.com>
2023-10-07 22:01:00 +0800

    #50 test our terrahome on terratowns test town (#51)

--- main.tf
+++ main.tf
@@ -24,31 +24,30 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="f39cf958-47a2-40c7-96cb-e8479413d33d" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid= var.teacherseat_user_uuid
+  token=var.terratowns_access_token
+
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   assets_path = var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home"{
-  name = "How to cook and bake!!"
+  name = "My HomeTown!!"
   description = <<DESCRIPTION
-  Cooking and baking are culinary techniques used to prepare a wide variety of foods.
-  Remember that cooking and baking can be both simple and complex, and 
-  there's a wide range of dishes you can prepare. As you gain experience, 
-  you'll become more comfortable with different techniques and flavors.
+  Tainan City is a special municipality in southern Taiwan facing the Taiwan Strait on its western coast. 
+  Tainan is the oldest city on the island and also commonly known as the "Capital City"[II] for its over 200 years of history
+  as the capital of Taiwan under Koxinga and later Qing rule
 DESCRIPTION
-#domain_name = module.terratowns_aws.cloudfront_url
-  domain_name = "af5511d.cloudfront.net"
-  town = "cooker-cove"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "af5511d.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
